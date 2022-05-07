3251c890fd8f37ca9a352bd3bb5db30c5cee6a05
orgr <or.grin@gmail.com>
2022-05-07 17:28:41 +0300

    Publish incoming messages to external running NATS, also subscribe to same subject to see whether it's working

--- server.go
+++ server.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+
+	"github.com/nats-io/nats.go"
 )
 
 const (
 	route = "sendMessage"
 	httpPort = "8080"
+	natsPort = nats.DefaultURL
+	subject = "messages"
 )
 
 type message struct {
@@ -17,10 +21,29 @@ type message struct {
 	Data string `json:"data"`
 }
 
+var natsClient nats.Conn
+
 func main() {
 	http.HandleFunc("/" + route, handler)
+	log.Println("Establising connection with NATS server")
+	natsConnectionPtr, err := nats.Connect(natsPort)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	natsClient = *natsConnectionPtr
+
+	subscribeToNats(&natsClient)
 	log.Println("Starting HTTP server")
 	log.Fatal(http.ListenAndServe(":" + httpPort, nil))
+}
+
+func subscribeToNats(nc *nats.Conn) {
+	nc.Subscribe(subject, func(msg *nats.Msg) {
+        // Print message data
+        data := string(msg.Data)
+        fmt.Println(data)
+    })
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -43,5 +66,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func publishMessage(message message) {
-
+	natsClient.Publish(subject, []byte(message.Data))
 }
